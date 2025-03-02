a40b6d9fcf187456b88e4e815eab96265e202f06
dumitrw <nubitrwgt@gmailcom>
2025-03-02 20:05:12 +0200

    modified:   astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx'; // Import corect pentru MDX
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
-import solidJs from '@astrojs/solid-js';
-import react from '@astrojs/react';
-
+import { defineConfig } from "astro/config"
+import mdx from "@astrojs/mdx"
+import sitemap from "@astrojs/sitemap"
+import tailwind from "@astrojs/tailwind"
+import solidJs from "@astrojs/solid-js"
+import react from '@astrojs/react'
 
 export default defineConfig({
   site: 'https://dumitrw.ro',
